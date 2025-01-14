494aa503c2fbf88fc6ce0346dd4cedd5e8a55781
profxj <xavier@ucolick.org>
2025-01-14 09:27:48 -0800

    functools

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup_keywords['install_requires'] = [
     'scikit-learn', 'scikit-image', 'tqdm', 'astropy', 'astropy-healpix',
     'healpy', 'cftime', 'bokeh', 'umap-learn', 'llvmlite', 'boto3',
     'xarray', 'h5netcdf',  'gsw',
-    'importlib-metadata', 'timm==0.3.2', 'functools']
+    'importlib-metadata', 'timm==0.3.2']
 setup_keywords['zip_safe'] = False
 setup_keywords['use_2to3'] = False
 setup_keywords['packages'] = find_packages()
